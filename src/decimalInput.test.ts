c89ea10f80954476280f428a8a9e354592897a6b
Dylan Lamont <didley.dev@gmail.com>
2023-07-31 19:39:26 +1000

    Increases test coverage

--- src/decimalInput.test.ts
+++ src/decimalInput.test.ts
@@ -1,84 +1,74 @@
 import { describe, expect, it } from 'vitest';
-import { SafeIntOrFloat, decimalInput } from './decimalInput';
+import { DecimalUtil, SafeIntOrFloat, decimalInput } from './decimalInput';
+
+const INVALID = { float: undefined, value: undefined, valid: false } as const;
 
 type DecimalInput = typeof decimalInput;
 type Params = Parameters<DecimalInput>;
 type Return = ReturnType<DecimalInput>;
+
 describe('decimalInput', () => {
-  it.each<[Params, Return]>([
-    // valid input
-    [['', { decimalPlaces: 2 }], { float: 0, value: '', valid: true }],
-    [['0.', { decimalPlaces: 2 }], { float: 0, value: '0.', valid: true }],
-    [['.', { decimalPlaces: 2 }], { float: 0, value: '0.', valid: true }],
-    [['.0', { decimalPlaces: 2 }], { float: 0, value: '0.0', valid: true }],
-    [
-      ['.01', { decimalPlaces: 2 }],
-      { float: 0.01, value: '0.01', valid: true },
-    ],
-    [
-      ['0.01', { decimalPlaces: 2 }],
-      { float: 0.01, value: '0.01', valid: true },
-    ],
-    [
-      ['-0.01', { decimalPlaces: 2 }],
-      { float: -0.01, value: '-0.01', valid: true },
-    ],
-    [
-      ['1.01', { decimalPlaces: 2 }],
-      { float: 1.01, value: '1.01', valid: true },
-    ],
-    [
-      ['11111.01', { decimalPlaces: 2 }],
-      { float: 11111.01, value: '11111.01', valid: true },
-    ],
-    [
-      [' ', { decimalPlaces: 2, min: 0, max: 1 }],
-      { float: 0, value: '', valid: true },
-    ],
-    [
-      [' 1', { decimalPlaces: 2, min: 0 }],
-      { float: 1, value: '1', valid: true },
-    ],
-    [
-      [' 1.1', { decimalPlaces: 2, max: 2 }],
-      { float: 1.1, value: '1.1', valid: true },
-    ],
-    // invalid input
-    [
-      ['1.01', { decimalPlaces: 1 }],
-      { float: undefined, value: undefined, valid: false },
-    ],
-    [
-      ['1.011', { decimalPlaces: 2 }],
-      { float: undefined, value: undefined, valid: false },
-    ],
-    [
-      ['1.0111', { decimalPlaces: 3 }],
-      { float: undefined, value: undefined, valid: false },
-    ],
-    [
-      ['$1', { decimalPlaces: 2 }],
-      { float: undefined, value: undefined, valid: false },
-    ],
-    [
-      ['1.1.1', { decimalPlaces: 2 }],
-      { float: undefined, value: undefined, valid: false },
-    ],
-    // min/max options
-    [
-      ['-1', { decimalPlaces: 2, min: 0 }],
-      { float: undefined, value: undefined, valid: false },
-    ],
-    [
-      ['-1', { decimalPlaces: 2, min: 10 }],
-      { float: undefined, value: undefined, valid: false },
-    ],
-    [
-      ['11', { decimalPlaces: 2, max: 10 }],
-      { float: undefined, value: undefined, valid: false },
-    ],
-  ])('\nInput: %s\nExpected:%s', (args, expected) => {
-    expect(decimalInput(...args)).toStrictEqual(expected);
+  describe('decimalInput()', () => {
+    it.each<[Params, Return]>([
+      // valid input
+      [['', { decimalPlaces: 2 }], { float: 0, value: '', valid: true }],
+      [['0.', { decimalPlaces: 2 }], { float: 0, value: '0.', valid: true }],
+      [['.', { decimalPlaces: 2 }], { float: 0, value: '0.', valid: true }],
+      [['.0', { decimalPlaces: 2 }], { float: 0, value: '0.0', valid: true }],
+      [
+        ['.01', { decimalPlaces: 2 }],
+        { float: 0.01, value: '0.01', valid: true },
+      ],
+      [
+        ['0.01', { decimalPlaces: 2 }],
+        { float: 0.01, value: '0.01', valid: true },
+      ],
+      [
+        ['-0.01', { decimalPlaces: 2 }],
+        { float: -0.01, value: '-0.01', valid: true },
+      ],
+      [
+        ['1.01', { decimalPlaces: 2 }],
+        { float: 1.01, value: '1.01', valid: true },
+      ],
+      [
+        ['11111.01', { decimalPlaces: 2 }],
+        { float: 11111.01, value: '11111.01', valid: true },
+      ],
+      [
+        [' ', { decimalPlaces: 2, min: 0, max: 1 }],
+        { float: 0, value: '', valid: true },
+      ],
+      [
+        [' 1', { decimalPlaces: 2, min: 0 }],
+        { float: 1, value: '1', valid: true },
+      ],
+      [
+        [' 1.1', { decimalPlaces: 2, max: 2 }],
+        { float: 1.1, value: '1.1', valid: true },
+      ],
+      [['00.'], { float: 0, value: '0.', valid: true }],
+      // invalid input
+      [['1.01', { decimalPlaces: 1 }], INVALID],
+      [['1.011', { decimalPlaces: 2 }], INVALID],
+      [['1.0111', { decimalPlaces: 3 }], INVALID],
+      [['$1', { decimalPlaces: 2 }], INVALID],
+      [['1.1.1', { decimalPlaces: 2 }], INVALID],
+      // min/max options
+      [['-1', { decimalPlaces: 2, min: 0 }], INVALID],
+      [['-1', { decimalPlaces: 2, min: 10 }], INVALID],
+      [['11', { decimalPlaces: 2, max: 10 }], INVALID],
+    ])('\nInput: %s\nExpected:%s', (args, expected) => {
+      expect(decimalInput(...args)).toStrictEqual(expected);
+    });
+  });
+  describe('validateFloat()', () => {
+    it.each<[Params, boolean]>([
+      // @ts-expect-error - it returns false on boolean input type
+      [[true], false],
+    ])('\nInput: %s\nExpected:%s', (args, expected) => {
+      expect(DecimalUtil.validateFloat(...args)).toBe(expected);
+    });
   });
 });
 
